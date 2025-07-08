7d26dd6675bc7af4b356cd1046e8e36a1ad35c2f
wtrmzt <134906718+wtrmzt@users.noreply.github.com>
2025-07-09 01:33:16 +0900

    Update vite.config.ts

--- frontend/knowledge-map-app/vite.config.ts
+++ frontend/knowledge-map-app/vite.config.ts
@@ -1,15 +1,13 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path" // Node.jsのpathモジュールをインポート
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // "@" を "src" ディレクトリへのパスにマッピング
-      "@": path.resolve(__dirname, "./src"), 
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
